00ceacc61dc311ca08c9556a6109b547d0ef48b0
Silviya <sbboteva@gmail.com>
2017-02-11 17:36:15 +0200

    slider done

--- web/js/site.js
+++ web/js/site.js
@@ -74,5 +74,26 @@ $(document).ready(function(){
     }
 
     //Init team slider 
+	if ($('#team-slider')) {
+        $('#team-slider').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            responsive: [{
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 540,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        });
+    }
 
 });
